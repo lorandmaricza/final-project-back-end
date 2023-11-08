22b55a9e8da48c14914c09bd6b9986df1fde62c0
Lorand Maricza <maricza.lorand@gmail.com>
2023-11-08 15:32:51 +0100

    reset insecure db connection for testing

--- database.php
+++ database.php
@@ -2,5 +2,22 @@
 session_start();
 error_reporting(E_ALL);
 
-$database = new PDO("mysql:host=".getenv('DATABASE_HOST').";dbname=".getenv('DATABASE_NAME'), getenv('DATABASE_USER'), getenv('DATABASE_PASS'));
+$DATABASE_HOST = '35.194.48.24';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'root';
+$DATABASE_NAME = 'project-db';
+
+$conn = mysqli_connect(
+    $DATABASE_HOST,
+    $DATABASE_USER,
+    $DATABASE_PASS,
+    $DATABASE_NAME
+);
+
+if (!$conn) {
+    die('Could not connect to the database: ' .
+        mysqli_error($conn));
+}
+
+//$database = new PDO("mysql:host=".getenv('DATABASE_HOST').";dbname=".getenv('DATABASE_NAME'), getenv('DATABASE_USER'), getenv('DATABASE_PASS'));
 
